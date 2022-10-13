37679e5ea3368231b3c692487873744e386b308a
Paulo Paracatu <pauloparacatu@kinsta.com>
2022-10-13 09:51:39 -0300

    fix port

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/ws.html');
 })
 
-app.listen(process.env.PORT, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
