938e5bbd26924097c5ce810ab27bc936718a14b8
Paulo Paracatu <pauloparacatu@kinsta.com>
2022-10-13 09:39:55 -0300

    fix port

--- ws.js
+++ ws.js
@@ -1,5 +1,5 @@
 var WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({port: 40510})
+  wss = new WebSocketServer({port: process.env.PORT})
 
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
